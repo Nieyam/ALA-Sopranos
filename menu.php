dc6524352506729d34842ed2e38fb8b642412b56
Nieyam <nieyambusiness@gmail.com>
2019-04-04 08:55:46 +0200

    titel

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Menu - Sopranos Pizza</title>
+    <title>Soprano's Menu</title>
 
     <!--Bootstrap Css and Main Css-->
     <link
